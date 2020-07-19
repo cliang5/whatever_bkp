3728ea4e2a339ac7ad4d59b648c40bf80104509d
Chen Liang <LCC@6Cores.local>
2020-07-19 10:36:44 -0400

    load the whole

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Angular APP';
+  todoArray=[];
+  todo;
+  addTodo(value) {
+    this.todoArray.push(value)
+    console.log(this.todoArray)
+  }
+  /*delete item*/  
+  deleteItem(todo){   
+    for(let i=0 ;i<= this.todoArray.length ;i++){    
+      if(todo== this.todoArray[i]){     
+        this.todoArray.splice(i,1)    
+      }   
+    }  
+  }  
+  // submit Form  
+  todoSubmit(value:any){ 
+    if(value!==""){    
+      this.todoArray.push(value.todo)     
+      //this.todoForm.reset()
+        }else{      
+          alert('Field required **')    
+        }
+  }
 }
